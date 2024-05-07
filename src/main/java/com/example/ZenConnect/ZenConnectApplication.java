b41e0733e27d605d855ad5048d3afd9e0c64b84d
misssizakele <misssizakele@gmail.com>
2024-05-07 14:21:00 +0200

    First commit

---
+++ src/main/java/com/example/ZenConnect/ZenConnectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ZenConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ZenConnect/ZenConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ZenConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZenConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
