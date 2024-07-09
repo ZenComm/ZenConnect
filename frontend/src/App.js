0baca84aab6196df225bd519c5e3f7421f757292
misssizakele <misssizakele@gmail.com>
2024-07-09 11:06:48 +0200

    Created profile page

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppRouter from './components/AppRouter';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div>
+      <AppRouter />
+    </div>
+  );
+}
+
+export default App;
